d9d0a8a6d97fe5b7a9a94c5fefad42056b7fedfb
Nick Girardo <nickgirardo@gmail.com>
2023-11-30 18:37:27 -0500

    Factor out system specific expressions

--- flake.nix
+++ flake.nix
@@ -3,17 +3,18 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
 
-  outputs = { self, nixpkgs }: 
-    let drv =
-      with import nixpkgs { system = "x86_64-linux"; };
+  outputs = { self, nixpkgs }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    drv = forAllSystems (system: 
+      with import nixpkgs { system = system; };
       stdenv.mkDerivation {
         name = "hello";
         src = self;
         buildPhase = "gcc -o hello ./main.c";
         installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
-      };
-  in {
-    packages.x86_64-linux.default = drv;
-    packages.x86_64-linux.hello = drv;
-  };
+      });
+    in {
+      packages = forAllSystems (system: { default = drv.${system}; hello = drv.${system}; });
+    };
 }
